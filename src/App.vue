1e052345ea70af1ed5e893c82bb34cb7e1737cf5
NINI <avatar12025@gmail.com>
2022-12-15 23:26:58 +0800

    add: props bind

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,49 @@
 <script>
   import messageItem from './components/messageItem.vue'
+  import messageItem2 from './components/messageItem2.vue'
+
 
   export default {
     components: {
       messageItem,
+      messageItem2
+    },
+    data() {
+      return {
+        message: [
+          {
+            id: 1,
+            sender: 'nini1',
+            msg: '今天吃麥當勞',
+            time: '12:00'
+          },
+          {
+            id: 2,
+            sender: 'nini2',
+            msg: '今天吃麥當勞',
+            time: '12:00'
+          },
+          {
+            id: 3,
+            sender: 'nini3',
+            msg: '今天吃麥當勞',
+            time: '12:00'
+          }
+        ]
+      }
     },
   }
 </script>
 
 <template>
   <messageItem sender="nini" msg="今天吃麥當勞" time="12:00"></messageItem>
+  <hr>
+  <!-- 動態獲取 -->
+  <messageItem2 v-for="msgList in message" :key="msgList.id" :sender="msgList.sender" :msg="msgList.msg"
+    :time="msgList.time"></messageItem2>
+
+  <hr>
+  <messageItem2 v-for="msgList in message" :key="msgList.id" v-bind="msgList"></messageItem2>
 </template>
 
 <style scoped>
---
+++ src/components/messageItem2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{sender}}</p>
+    <p>{{time}}</p>
+    <p>{{msg}}</p>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    props: ["sender", "time", "msg"],
+    data() {
+      return {
+
+      }
+    },
+  }
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
