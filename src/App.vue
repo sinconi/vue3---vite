07cd3f0857d8d5fd1fc88c40b7cc385811056a60
NINI <avatar12025@gmail.com>
2022-12-15 23:05:29 +0800

    add: props

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import messageItem from './components/messageItem.vue'
+
+  export default {
+    components: {
+      messageItem,
+    },
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <messageItem sender="nini" msg="今天吃麥當勞" time="12:00"></messageItem>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/messageItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{sender}}</p>
+    <p>{{time}}</p>
+    <p>{{msg}}</p>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    props: ["sender", "time", "msg"]
+  }
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
