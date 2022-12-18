42277dd0a316c6cd24d75a3d00e3123d9d4e6615
NINI <avatar12025@gmail.com>
2022-12-18 16:36:50 +0800

    add: props class

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script>
   import messageItem from './components/messageItem.vue'
   import messageItem2 from './components/messageItem2.vue'
+  import blogPostItem from './components/blogPostItem.vue'
+
 
 
   export default {
     components: {
       messageItem,
-      messageItem2
+      messageItem2,
+      blogPostItem
     },
     data() {
       return {
@@ -29,6 +32,23 @@
             msg: '今天吃麥當勞',
             time: '12:00'
           }
+        ],
+        posts: [
+          {
+            id: 1,
+            title: 'post1',
+            link: 'https://www.google.com/'
+          },
+          {
+            id: 2,
+            title: 'post2',
+            link: 'https://www.google.com/'
+          },
+          {
+            id: 3,
+            title: 'post3',
+            link: 'https://www.google.com/'
+          },
         ]
       }
     },
@@ -36,14 +56,20 @@
 </script>
 
 <template>
-  <messageItem sender="nini" msg="今天吃麥當勞" time="12:00"></messageItem>
-  <hr>
+  <!-- <messageItem sender="nini" msg="今天吃麥當勞" time="12:00"></messageItem> -->
+  <!-- <hr> -->
   <!-- 動態獲取 -->
-  <messageItem2 v-for="msgList in message" :key="msgList.id" :sender="msgList.sender" :msg="msgList.msg"
-    :time="msgList.time"></messageItem2>
+  <!-- <messageItem2 v-for="msgList in message" :key="msgList.id" :sender="msgList.sender" :msg="msgList.msg"
+    :time="msgList.time"></messageItem2> -->
 
-  <hr>
-  <messageItem2 v-for="msgList in message" :key="msgList.id" v-bind="msgList"></messageItem2>
+  <!-- <hr> -->
+  <!-- <messageItem2 v-for="msgList in message" :key="msgList.id" v-bind="msgList"></messageItem2> -->
+
+  <div>
+    <!-- 在組件上設置class相當於直接對此元件最外層添增class屬性 -->
+    <blogPostItem v-for="post in posts" :key="post.id" v-bind="post" class="blogLink"></blogPostItem>
+  </div>
+
 </template>
 
 <style scoped>
---
+++ src/components/blogPostItem.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    <a :href="link">{{title}}</a>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['link', 'title']
+  }
+</script>
+<style lang="">
+
+</style>
